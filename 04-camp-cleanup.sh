946d71590987ae2090e70cd21e99a0286c06635d
Sam Tracey <sam@samtracey.co.uk>
2022-12-16 21:06:09 +0000

    add a comment

--- 04-camp-cleanup.sh
+++ 04-camp-cleanup.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/awk -f
+
+# change the field separator
 BEGIN {FS="[,-]"}
 {if ($1 && (($1 <= $3 && $2 >= $4 ) || ($1 >= $3 && $2 <= $4 ))) { contained++}}
 END {print "contained:", contained}
